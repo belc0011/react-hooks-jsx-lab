bd50085d45a734bd985a5523b3b537fc48a29a34
Monica Garcia <monicairenegarcia@gmail.com>
2024-01-09 14:55:03 -0700

    completed jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I have been married 17 years, I have two amazing boys, and I can't wait to start my career as a developer!</p>
+    <img src={image} alt='I made this'/>
+  </div>
+  )
 }
 
 export default About;
